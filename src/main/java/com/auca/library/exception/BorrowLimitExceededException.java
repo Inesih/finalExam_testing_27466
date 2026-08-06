4835414c5e7a5689b70732da7e0124bcae42d070
Ines <ihirwenzayisengaines@gmail.com>
2026-08-07 00:07:34 +0300

    Add BorrowLimitExceededException class

--- src/main/java/com/auca/library/exception/BorrowLimitExceededException.java
+++ src/main/java/com/auca/library/exception/BorrowLimitExceededException.java
@@ -1,1 +1,7 @@
+package com.auca.library.exception;
 
+public class BorrowLimitExceededException extends RuntimeException {
+    public BorrowLimitExceededException(String message) {
+        super(message);
+    }
+}
