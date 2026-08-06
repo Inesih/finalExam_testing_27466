251f8d732de3716185a5194a1e83a60739ce7b74
Ines <ihirwenzayisengaines@gmail.com>
2026-08-07 00:00:36 +0300

    Add files via upload

---
+++ src/main/java/com/auca/library/domain/EMembershipStatus.java
@@ -0,0 +1,5 @@
+package com.auca.library.domain;
+
+public enum EMembershipStatus {
+    PENDING, ACTIVE
+}
---
+++ src/main/java/com/auca/library/domain/MembershipTierConstants.java
@@ -0,0 +1,15 @@
+package com.auca.library.domain;
+
+public class MembershipTierConstants {
+    public static final String GOLD = "Gold";
+    public static final int GOLD_MAX_BOOKS = 5;
+    public static final int GOLD_DAILY_RATE = 50;
+
+    public static final String SILVER = "Silver";
+    public static final int SILVER_MAX_BOOKS = 3;
+    public static final int SILVER_DAILY_RATE = 30;
+
+    public static final String STRIVER = "Striver";
+    public static final int STRIVER_MAX_BOOKS = 2;
+    public static final int STRIVER_DAILY_RATE = 10;
+}
